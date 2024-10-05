ea52331f176b5cb48b195bf7a1729acafff9f591
dmadoery <daniel@madoery.com>
2024-10-05 14:12:17 +0200

    deleted files finally

--- Consumer.java
+++
@@ -1,5 +0,0 @@
-public class Consumer {
-    public static void main(String[] args) {
-       
-    }
-}
--- Producer.java
+++
@@ -1,5 +0,0 @@
-public class Producer {
-    public static void main(String[] args) {
-       
-    }
-}
