2c3b92f3f3b64af2aee924cfdf4f0249f6f2282e
dmadoery <daniel@madoery.com>
2024-10-05 14:08:10 +0200

    deleted files and added maven project

---
+++ Benchmark/src/main/java/org/example/DataGenerator.java
@@ -0,0 +1,55 @@
+package org.example;
+import static java.lang.System.in;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.util.Arrays;
+
+
+public class DataGenerator {
+
+    public Object obj;
+
+    public static String[] split(String line) {
+        String[] splited = line.split(";", 10);
+        return splited;
+    }
+
+
+    public static void main(String[] args) throws  Exception {
+        String[][] data = new String[20500][5];
+        Sensor[] frontLeftTyre = new Sensor[20500];
+        try(FileReader in = new FileReader("src/main/resources/frontLeftTyre.csv");
+            BufferedReader br = new BufferedReader(in)) {
+            int j = 0;
+            while(true) {
+                String line = br.readLine();
+                if(line == null) {
+                    break;
+                }
+                if(line.isEmpty() || line.startsWith("#")) {
+                    continue;
+                }
+                else{
+                    String[] splited = split(line);
+                    //System.out.println(splited.length);
+                    System.arraycopy(splited, 0, data[j], 0, splited.length);
+                    // System.out.println(Arrays.toString(data[j]));
+                    // System.out.println(data.length);
+                    for (String item : data[j]) {
+                        System.out.println("Row "+ j + ": " + item);
+                    }
+                    if (j > 0) {
+                        int l = data[j].length - 1;
+                        frontLeftTyre[j] = new Sensor(data[j][l-4], data[j][l-3], data[j][l-2], Double.parseDouble(data[j][l-1]), Double.parseDouble(data[j][l]));
+                    } else {
+                        frontLeftTyre[j] = new Sensor("test", "test", "test", 12.555, 24.555);
+                    }
+                    frontLeftTyre[j].print();
+
+                }
+                j++;
+            }
+            br.close();
+        }
+    }
+}
---
+++ Benchmark/src/main/java/org/example/SensorGenerator.java
@@ -0,0 +1,51 @@
+package org.example;
+
+import com.opencsv.CSVWriter;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Date;
+
+public class SensorGenerator {
+    //header
+    private final String[] header;
+    ArrayList<Object> data = new ArrayList <>();
+
+    public SensorGenerator(String name, String data1, String data2) {
+        header = new String[] {name, "id", "date", "time", data1, data2};
+    }
+
+    public static void main(String[] args) {
+        SensorGenerator sensor1 = new SensorGenerator("test", "temp", "pressure");
+        File file = new File("src/main/resources/" + sensor1.header[0] + ".csv");
+        try {
+            // create FileWriter object with file as parameter
+            FileWriter outputfile = new FileWriter(file);
+
+            // create CSVWriter object filewriter object as parameter
+            CSVWriter writer = new CSVWriter(outputfile);
+
+            // adding header to csv
+            writer.writeNext(sensor1.header);
+
+            //create data
+            Date date = new Date();
+            System.out.println(date);
+
+            // add data to csv
+            String[] data1 = { "Aman", "10", "620" };
+            writer.writeNext(data1);
+            String[] data2 = { "Suraj", "10", "630" };
+            writer.writeNext(data2);
+
+            // closing writer connection
+            writer.close();
+        }
+        catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+    }
+}
